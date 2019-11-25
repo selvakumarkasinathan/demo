683b7952b6198cf70ea47e74e181a30a5d1a405f
selvakumarkasinathan <44283179+selvakumarkasinathan@users.noreply.github.com>
2019-11-25 22:52:09 +0530

    Feature branch

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo1Application {
-
+//System.Out.println("Test");
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
